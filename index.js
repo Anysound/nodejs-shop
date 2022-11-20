a61560dd5b5d7f5721abe5787cb576101a27ac31
anysound <anysoundspb@gmail.com>
2022-11-20 13:54:33 +0300

    init backend

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const tgBot = require("node-telegram-bot-api");
 const token = `5395482139:AAFeebmrdWKh_YfGPUl3rGmAm8gBRA1--wc`;
-
+const express = require("express");
+const cors = require("cors");
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new tgBot(token, { polling: true });
-const webAppUrl = "https://ya.ru";
+const app = express();
+app.use(express.json()); // middleware for parsing json
+app.use(cors()); // cross-domain queries
+
+const webAppUrl = "https://fancy-centaur-b5ff64.netlify.app/";
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on("message", async (msg) => {
@@ -13,7 +18,7 @@ bot.on("message", async (msg) => {
     await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
       reply_markup: {
         inline_keyboard: [
-          [{ text: "Заполнить форму", web_app: { url: webAppUrl } }],
+          [{ text: "Заполнить форму", web_app: { url: webAppUrl + "form" } }],
         ],
       },
     });
@@ -26,6 +31,46 @@ bot.on("message", async (msg) => {
       },
     });
   }
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      console.log(data);
+      await bot.sendMessage(chatId, "Спасибо за обратную связь!");
+      await bot.sendMessage(chatId, "Ваша страна: " + data?.country);
+      await bot.sendMessage(chatId, "Ваша улица: " + data?.street);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, "Received your message");
 });
+
+app.post("/web-data", async (req, res) => {
+  const { queryId, products, totalPrice } = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Успешная покупка",
+      inpout_message_content: {
+        message_text:
+          "Поздравляю с покупкой, Вы приобрели товар на сумму " + totalPrice,
+      },
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Не удалось приобрести товары",
+      inpout_message_content: {
+        message_text: "Не удалось приобрести товары, ошибка: " + e,
+      },
+    });
+    return res.status(500).json({});
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("server started on port ", PORT));
